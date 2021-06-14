3d5f76b7d124c2b7bdd7c5505036d7d81fe8e70e
Sky-Tex <39876602+Sky-Tex@users.noreply.github.com>
2021-06-14 17:07:17 +0200

    fix

--- js/cartnumber.js
+++ js/cartnumber.js
@@ -1,12 +1,12 @@
 function cartNumber() {
-    const panier = JSON.parse(localStorage.getItem("panier"));
-  
-    if (panier) {
-      let inCart = 0;
-      panier.forEach(() => {
-        inCart = inCart + 1;
-      });
-      localStorage.setItem("inCart", inCart);
-      document.getElementById("cart_number").textContent = inCart;
-    }
+  const panier = JSON.parse(localStorage.getItem("panier"));
+
+  if (panier) {
+    let inCart = 0;
+    panier.forEach(() => {
+      inCart = inCart + 1;
+    });
+    localStorage.setItem("inCart", inCart);
+    document.getElementById("cart_number").textContent = inCart;
   }
+}
--- js/home.js
+++ js/home.js
@@ -26,5 +26,5 @@ function getRequest(){
 	}
 
 	
-	getRequest()
-	cartNumber()
+	getRequest();
+	cartNumber();
